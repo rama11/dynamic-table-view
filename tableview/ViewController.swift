1156007a50c98eeb079510b6a6ad8d59941d9363
rama11 <prof.agastyo@gmail.com>
2020-07-08 14:21:21 +0700

    Initial Commit

---
+++ tableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableview
+//
+//  Created by Sinergy on 7/8/20.
+//  Copyright © 2020 Sinergy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
